55622529b2f67262f322884dc061a3ce80a95916
Fernando Rucci <feraruc@hotmail.com>
2026-06-05 01:27:27 -0300

    Modificacion_feedback

--- sistema_logistico.cpp
+++ sistema_logistico.cpp
@@ -15,103 +15,87 @@ void productor(Sistema& s, int cant) {
         p.prioridad = rand() % 2;
         p.fecha_creacion = tiempo_ms();
 
-        wait(s.mtx_id);
+        s.mtx_id.lock(); // Uso de Mutex en vez de wait
         p.id = ++s.id_global;
         s.producidos++;
-        signal(s.mtx_id);
+        s.mtx_id.unlock(); // Uso de Mutex en vez de signal
 
         std::this_thread::sleep_for(std::chrono::milliseconds(90));
 
-        wait(s.mtx_estanteria);
-        s.wq.push(p);
-        signal(s.mtx_estanteria);
+        s.mtx_estanteria.lock();
+        s.wq.push_back(p); // Ingresa al final del arreglo (Cola)
+        s.mtx_estanteria.unlock();
 
-        signal(s.hay_paquetes);
+        signal(s.hay_paquetes); // Semáforo contador para avisar
     }
 }
 
 void consumidor(Sistema& s) {
     while (true) {
-        Paquete p_transf;
-        Paquete p_proc;
+        Paquete p_transf, p_proc;
         bool transferir = false, procesar = false;
 
-        wait(s.mtx_estanteria);
+        s.mtx_estanteria.lock();
 
         if (s.fin && s.wq.empty() && s.pq.empty()) {
-            signal(s.mtx_estanteria);
+            s.mtx_estanteria.unlock();
             break;
         }
 
         long long ahora = tiempo_ms();
 
-        // 1 Se evalua la cinta
+        // 1. EVALUACIÓN DE LA CINTA
         if (!s.pq.empty() && (ahora - s.registro_cinta[s.pq.front().id] >= 550)) {
             p_proc = s.pq.front();
-            s.pq.pop();
+            s.pq.erase(s.pq.begin()); // Extrae limpiamente el primero (FIFO)
             procesar = true;
         }
 
-        // Se evalua la estanteria
+        // 2. EVALUACIÓN DE LA ESTANTERÍA (Recorrido limpio sin pop/push)
         else if (s.pq.size() < 5 && !s.wq.empty()) {
-            int target_id = -1;
-            int wq_size = s.wq.size();
+            
+            int indice_elegido = -1;
 
-            // A. Buscar Inanici�n (>= 6000ms) Se evalua el tiempo desde su creacion y esto
-            // va como prioridad por sobre el orden, segun la consigna
-            for (int i = 0; i < wq_size; i++) {
-                Paquete p = s.wq.front();
-                s.wq.pop();
-
-                if (target_id == -1 && p.prioridad == 0 && (ahora - p.fecha_creacion >= 6000)) {
-                    target_id = p.id;
+            // A. Buscar Inanición (Miro los elementos directamente)
+            for (size_t i = 0; i < s.wq.size(); ++i) {
+                if (s.wq[i].prioridad == 0 && (ahora - s.wq[i].fecha_creacion >= 6000)) {
+                    indice_elegido = i;
+                    break; 
                 }
-                s.wq.push(p);
             }
 
-            // B. Buscar Prioridad Alta (1)
-            if (target_id == -1) {
-                for (int i = 0; i < wq_size; i++) {
-                    Paquete p = s.wq.front();
-                    s.wq.pop();
-
-                    if (target_id == -1 && p.prioridad == 1) {
-                        target_id = p.id;
+            // B. Si no hay inanición, busco Prioridad Alta
+            if (indice_elegido == -1) {
+                for (size_t i = 0; i < s.wq.size(); ++i) {
+                    if (s.wq[i].prioridad == 1) {
+                        indice_elegido = i;
+                        break;
                     }
-                    s.wq.push(p);
                 }
             }
 
-            // C. Cuando no se encontro nada por Prioirdad o Inanicion se desencola de manera normal
-            if (target_id == -1) {
-                target_id = s.wq.front().id;
+            // C. Si no hubo nada especial, saco el primero (FIFO)
+            if (indice_elegido == -1) {
+                indice_elegido = 0;
             }
 
-            // D. EXTRAER el paquete elegido (Tercera y �ltima rotaci�n)
-            for (int i = 0; i < wq_size; i++) {
-                Paquete p = s.wq.front();
-                s.wq.pop();
-
-                if (!transferir && p.id == target_id) {
-                    p_transf = p;
-                    transferir = true;
-                } else {
-                    s.wq.push(p);
-                }
-            }
+            // D. EXTRAER el paquete elegido
+            p_transf = s.wq[indice_elegido];
+            s.wq.erase(s.wq.begin() + indice_elegido); // Borra solo ese elemento
+            transferir = true;
         }
 
-        // 3 Espera Pasiva
+        // 3. ESPERA PASIVA
         else {
             if (s.pq.empty()) {
-                signal(s.mtx_estanteria);
-                wait(s.hay_paquetes);
+                s.mtx_estanteria.unlock();
+                wait(s.hay_paquetes); // El hilo se duerme
             } else {
                 int id_primero = s.pq.front().id;
                 long long faltante = 550 - (ahora - s.registro_cinta[id_primero]);
-
-                signal(s.mtx_estanteria);
-
+                
+                s.mtx_estanteria.unlock();
+                
                 if (faltante > 0) {
                     std::this_thread::sleep_for(std::chrono::milliseconds(faltante));
                 }
@@ -119,55 +103,49 @@ void consumidor(Sistema& s) {
             continue;
         }
 
-        signal(s.mtx_estanteria);
+        s.mtx_estanteria.unlock();
 
-        // Acciones del Consumidor
-
+        // ACCIONES FÍSICAS
         if (transferir) {
-            wait(s.mtx_cinta);
+            s.mtx_cinta.lock();
             std::this_thread::sleep_for(std::chrono::milliseconds(420));
 
-            wait(s.mtx_estanteria);
-            s.pq.push(p_transf);
-            s.registro_cinta[p_transf.id] = tiempo_ms();
-            signal(s.mtx_estanteria);
+            s.mtx_estanteria.lock();
+            s.pq.push_back(p_transf); 
+            s.registro_cinta[p_transf.id] = tiempo_ms(); 
+            s.mtx_estanteria.unlock();
 
-            signal(s.mtx_cinta);
+            s.mtx_cinta.unlock();
             signal(s.hay_paquetes);
         }
 
         if (procesar) {
-            wait(s.mtx_cinta);
+            s.mtx_cinta.lock();
             std::this_thread::sleep_for(std::chrono::milliseconds(270));
 
             long long tiempo_total = tiempo_ms() - p_proc.fecha_creacion;
 
-            wait(s.mtx_estanteria);
-            s.registro_cinta.erase(p_proc.id);
+            s.mtx_estanteria.lock();
+            s.registro_cinta.erase(p_proc.id); 
 
             if (p_proc.prioridad == 1) {
                 s.proc_alta++; s.espera_alta += tiempo_total;
             } else {
                 s.proc_baja++; s.espera_baja += tiempo_total;
             }
-            signal(s.mtx_estanteria);
+            s.mtx_estanteria.unlock();
 
-            signal(s.mtx_cinta);
+            s.mtx_cinta.unlock();
         }
     }
 }
 
-// Funcion Ejecutar : Se utiliza para lanzar los hilos automaticamente y dormirlos
-// de modo que en el main para porbar los escenarios quede sean mas faciles de manejar pasandoles la cantidad
-// de productores y consumidores
-// y la cantidad de paquetes
+// FUNCIÓN EJECUTAR
 void ejecutar(int n_prod, int n_cons, int pqts, const std::string& nombre) {
     Sistema s;
 
-    // Inicializaci�n de nuestros sem�foros (Mutex en 1, Contador en 0)
-    init(s.mtx_estanteria, 1);
-    init(s.mtx_cinta, 1);
-    init(s.mtx_id, 1);
+    // Ya no inicializamos los mutex acá porque std::mutex se inicializa solo.
+    // Solo inicializamos el semáforo contador en 0.
     init(s.hay_paquetes, 0);
 
     std::vector<std::thread> h_cons, h_prod;
@@ -175,27 +153,21 @@ void ejecutar(int n_prod, int n_cons, int pqts, const std::string& nombre) {
     std::cout << "Ejecutando: " << nombre << "...\n";
     std::cout << "======================================================\n";
 
-    // 1. Arrancan los hilos mediante un for
     for (int i = 0; i < n_cons; ++i) h_cons.emplace_back(consumidor, std::ref(s));
     for (int i = 0; i < n_prod; ++i) h_prod.emplace_back(productor, std::ref(s), pqts);
 
-    // 2. Se los hace termianr su turno con .join
     for (auto& t : h_prod) t.join();
 
-    // 3. Se avisa a los consumidores que termino el proceso modificando la variable protegida fin
-    wait(s.mtx_estanteria);
+    s.mtx_estanteria.lock();
     s.fin = true;
-    signal(s.mtx_estanteria);
+    s.mtx_estanteria.unlock();
 
-    // Despertamos a cualquier consumidor que se haya quedado dormido esperando paquetes
     for(int i = 0; i < n_cons * 5; i++) {
         signal(s.hay_paquetes);
     }
 
-    // 4. ESPERAMOS A QUE LOS CONSUMIDORES TERMINEN DE VACIAR LAS COLAS
     for (auto& t : h_cons) t.join();
 
-    // 5. Calculo e impresion de las Metricas
     std::cout << "Metricas obligatorias:\n"
               << "- Cantidad total de paquetes producidos: " << s.producidos << "\n";
 
--- sistema_logistico.h
+++ sistema_logistico.h
@@ -2,10 +2,10 @@
 #define SISTEMA_LOGISTICO_H
 
 #include "semaforo.h"
-#include <queue>
+#include <vector>
 #include <map>
 #include <string>
-
+#include <mutex> // Usamos Mutex reales como pidió el profe
 
 struct Paquete {
     int id;
@@ -14,16 +14,20 @@ struct Paquete {
 };
 
 struct Sistema {
-    //"Estructuras de datos, se aconsejo usar dos colar comunes
+    // Usamos arreglos dinámicos, tratados lógicamente como colas (FIFO).
+    // Esto permite iterar sin hacer pop/push.
+    std::vector<Paquete> wq; 
+    std::vector<Paquete> pq; 
 
-    std::queue<Paquete> wq; // Buffer 1: Waiting Queue
-    std::queue<Paquete> pq; // Buffer 2: Processing Queue
-
-    // Para hacer mapeo en general
     std::map<int, long long> registro_cinta;
 
-    // Semaforos
-    Semaforo mtx_estanteria, mtx_cinta, mtx_id, hay_paquetes;
+    // Mutex para exclusión mutua estricta de las variables compartidas
+    std::mutex mtx_estanteria;
+    std::mutex mtx_cinta;
+    std::mutex mtx_id;
+    
+    // Semáforo común (contador) SOLO para la sincronización de hilos dormidos
+    Semaforo hay_paquetes;
 
     int id_global = 0, producidos = 0, proc_alta = 0, proc_baja = 0;
     long long espera_alta = 0, espera_baja = 0;
